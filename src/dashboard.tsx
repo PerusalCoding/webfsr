68b35b396f02b0acce979ac7100bf1764e270a27
perusalcoding <perusalusa88@gmail.com>
2026-08-29 21:46:54 -0700

    UI size correction

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -3095,7 +3095,7 @@ const Dashboard = () => {
 						onDragEnd={sensorBarsDrag.handleDragEnd}
 					/>
 				</div>
-				<div className="relative flex-1 min-h-0">
+				<div className="relative flex-1 min-h-0 overflow-hidden">
 					<SensorBar
 						key={`sensor-${index}`}
 						value={latestData?.values[index] || 0}
